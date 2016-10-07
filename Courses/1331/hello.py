ed839f6150f3bc9fa2b7a2e75798cb9db952a613
Cullen Newsom <electrodynatronic@gmail.com>
2016-10-07 10:20:27 -0500

    Create hello.py

---
+++ Courses/1331/hello.py
@@ -0,0 +1,12 @@
+############################################
+#
+# Name: Newsom
+# Course: 1331
+# Origin Date: 8/31/2016
+# Description: Hello World
+# This program outputs "Hello World" when run
+#
+############################################
+
+# The next line should print "Hello World" when the program is run
+print("Hello World")
